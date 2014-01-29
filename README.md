db29c2d07a1c91401163aa5fac8345f10ed851b7
Rostislav Kondratenko <r.kondratenko@wwpass.com>
2014-01-29 14:48:56 +0400

    ADD: setup.py

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# Python SDK
+# WWPass Python SDK
 
 example — pure python http server (getPUID example)
+wwpass.py — WWPass Python library
+
+To install run:
+#python setup.py install
---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='WWPass',
+    version='1.3',
+    description='WWPASS Access Library',
+    author='WWPASS Corporation',
+    author_email='support@wwpass.com',
+    maintainer="Rostislav Kondratenko",
+    maintainer_email="r.kondratenko@wwpass.com",
+    py_modules=['wwpass'],
+)
+
